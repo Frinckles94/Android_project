dbbe0ef62a31d3b14daaac7fd79e3f4922ab1f1d
Ilja Kozirevs <frinckles94@gmail.com>
2023-05-09 21:45:57 +0300

    added validation, perhaps found layout dimensions,

--- app/src/main/java/ludf/ik17255/myapplication/MainActivity.java
+++ app/src/main/java/ludf/ik17255/myapplication/MainActivity.java
@@ -12,12 +12,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
+    private EditText editTextNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +27,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton = findViewById(R.id.mButton);
+        editTextNumber = findViewById(R.id.editTextNumber);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                myView v = new myView(MainActivity.this);
-                Bitmap result = Bitmap.createBitmap(25, 25, Bitmap.Config.ARGB_8888);
-                Canvas canvas1 = new Canvas(result);
-                v.draw(canvas1);
-                ConstraintLayout l = findViewById(R.id.drawLayout);
-                l.addView(v);
+                String input = editTextNumber.getText().toString();
+                if(input.length() > 0){
+                    float scale = Float.parseFloat(input);
+
+                    if(scale > 4 || scale < 0.5){
+                        editTextNumber.setError("Number should be greater than 0.5 and less than 4");
+                    }else{
+
+                        myView v = new myView(MainActivity.this);
+                        int height = findViewById(R.id.drawLayout).getMeasuredHeight();
+                        int width = findViewById(R.id.drawLayout).getMeasuredWidth();
+                        int padding = 10;
+
+                        Bitmap result = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                        Canvas canvas1 = new Canvas(result);
+                        v.draw(canvas1);
+                        ConstraintLayout l = findViewById(R.id.drawLayout);
+                        l.addView(v);
+
+
+                        //mButton.setText(String.valueOf(scale));
+                        mButton.setText(String.valueOf(height));
+                    }
+                }else{
+                    editTextNumber.setError("Please enter a number");
+                }
+
             }
         });
 
